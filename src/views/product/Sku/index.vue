0f15c9c6f47b4d8397f6499b7efbcb72204c3469
NaerDD <806915230@qq.com>
2023-01-26 04:20:50 +0800

    2023/1/26 4:20 我还要偷窥你的生活多久

--- src/views/product/Sku/index.vue
+++ src/views/product/Sku/index.vue
@@ -10,9 +10,9 @@
           <img :src="row.skuDefaultImg" alt="" style="width:80px;height:80px">
         </template>
       </el-table-column>
-      <el-table-column prop="weight"   label="重量" width="width"></el-table-column>
-      <el-table-column prop="price"   label="价格" width="width"></el-table-column>
-      <el-table-column prop="prop"   label="操作" width="width">
+      <el-table-column prop="weight" label="重量" width="80"></el-table-column>
+      <el-table-column prop="price" label="价格" width="80"></el-table-column>
+      <el-table-column prop="prop" label="操作" width="width">
         <template slot-scope="{row,$index}">
           <el-button type="success" icon="el-icon-sort-down" size="mini" v-if="row.isSale==1" @click="down(row)"></el-button>
           <el-button type="success" icon="el-icon-sort-up" size="mini" v-else @click="up(row)"></el-button>
@@ -36,8 +36,38 @@
     </el-pagination>
 
     <!-- 抽屉效果 -->
-    <el-drawer :visible.sync="show" :before-close="handleClose"> 
-      <span>我来啦!</span>
+    <el-drawer :visible.sync="show" :show-close="false" size="35%" > 
+      <el-row>
+      <el-col :span="5">名称</el-col>
+      <el-col :span="16">{{skuInfo.skuName}}</el-col>
+    </el-row>
+      <el-row>
+      <el-col :span="5">描述</el-col>
+      <el-col :span="16">{{skuInfo.skuDesc}}</el-col>
+    </el-row>
+      <el-row>
+      <el-col :span="5">价格</el-col>
+      <el-col :span="16">{{skuInfo.price}}元</el-col>
+    </el-row>
+      <el-row>
+      <el-col :span="5">平台属性</el-col>
+      <el-col :span="16">
+        <template>
+          <el-tag type="success" :key="attr.id" v-for="(attr,index) in skuInfo.skuAttrValueList" style="margin-right:10px"
+          >{{attr.attrId0}}-{{attr.valueId}}</el-tag>
+        </template>
+      </el-col>
+    </el-row>
+      <el-row>
+      <el-col :span="5">商品图片</el-col>
+      <el-col :span="16">
+        <el-carousel height="150px">
+            <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+            <img :src="item.imgUrl" alt="" > 
+            </el-carousel-item>
+        </el-carousel>
+      </el-col>
+    </el-row>
     </el-drawer>
     
   </div>
@@ -102,15 +132,48 @@ export default {
     //查看详情
     async getSkuInfo(sku){
       let result = await this.$API.sku.reqSkuById(sku.id);
+      //展示抽屉
+      this.show = true;
       // console.log(result);
       if(result.code==200){
-        this.skuInfo=result.data;
+        this.skuInfo = result.data;
       }
-    }
+    },
 }
 }
 </script>
 
-<style>
+<style scoped>
+  .el-row .el-clo-5{
+    font-size: 18px;
+    text-align: right;
+  }
+  .el-clo{
+    margin: 10px 10px;
+  }
+</style>
 
+<style>
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 150px;
+    margin: 0;
+  }
+
+  .el-carousel__item:nth-child(2n) {
+     background-color: #99a9bf;
+  }
+  
+  .el-carousel__item:nth-child(2n+1) {
+     background-color: #d3dce6;
+  }
+
+  >>>.el-carousel_button{
+    width: 10px;
+    height: 10px;
+    background: red;
+    border-radius: 50%;
+  }
 </style>
